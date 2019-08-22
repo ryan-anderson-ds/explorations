76a4ec114894a8081f4e38fa508f5e6259c9b7da
Ryan Anderson <ryan.anderson@booking.com>
2019-08-22 15:25:37 +0200

    hunga bunga

---
+++ film_success/film_actors_to_revenues.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 4 10:54:52 2019
+@author: rian-van-den-ander
+"""
+
+import pandas as pd
+from encode_json_column import encode_json_column
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+dataset = pd.read_csv('tmdb_5000_movies.csv')
+dataset_credits = pd.read_csv('tmdb_5000_credits.csv')
+dataset = pd.concat([dataset, dataset_credits], axis=1)
+
+dataset = encode_json_column(dataset, 22,"name", 500, 1)
+
+y = dataset.iloc[:, 12].values #12 for revenue, 18 for rating
+X = dataset.iloc[:, 23:].values
+X_names = dataset.columns[23:].values
+
+from xgboost import XGBRegressor
+regressor = XGBRegressor(colsample_bytree= 0.6, gamma= 0.7, max_depth= 4, min_child_weight= 5,
+                         subsample = 0.8, objective='reg:squarederror')
+regressor.fit(X, y)
+
+importances = {}
+
+count = 0
+for feature_importance in regressor.feature_importances_:
+    if feature_importance > 0.002:
+        feature_name = X_names[count]
+        importances[feature_name] = feature_importance
+    count+=1
+    
+import operator
+sorted_importances = sorted(importances.items(), key=operator.itemgetter(1), reverse=True)
+sorted_importances = sorted_importances[0:40]
+
+xs, ys = [*zip(*sorted_importances)]
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+plt.barh(xs, ys)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_title('Top 40 cast and crew that predict movie revenue' )
+plt.xlabel('Feature importance score')
+plt.show()
---
+++ personality_traits/personality_traits_hunga_bunga.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 14:34:59 2019
+
+@author: User
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 17:14:00 2019
+
+@author: rian-van-den-ander
+"""
+
+#!/usr/bin/env python -W ignore::DeprecationWarning
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('personality_data.csv', header=0, sep='\t')
+
+dataset = dataset.dropna() #drop any null data
+dataset = dataset[dataset.age < 100] # Removing bogus age
+dataset = dataset[dataset.gender.isin([1,2])] # removing non specific genders
+dataset = dataset[dataset.accuracy > np.percentile(dataset.accuracy,5)] # removing very low accuracies
+dataset = dataset[dataset.accuracy <= 100] # Removing very high accuracies
+dataset = dataset[dataset.elapsed <= 5000] # Removing very high accuracies
+dataset = dataset[dataset.elapsed > 300] # Removing very high accuracies
+
+X = dataset.iloc[:,0:-6].values
+y_age = dataset.iloc[:,-6].values
+y_gender = dataset.iloc[:,-5].values
+
+# split into training and test data
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_gender_train, y_gender_test = train_test_split(X, y_gender, test_size = 0.1)
+
+from hunga_bunga import HungaBungaClassifier, HungaBungaRegressor
+
+clf = HungaBungaClassifier(brain=True)
+clf.fit(X_train, y_gender_train)
+
+
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_gender_test, y_pred)
