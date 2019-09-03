c406e39ce53e9c21bd358cfba1468ab5b8381c8f
rian-van-den-ander <52826135+rian-van-den-ander@users.noreply.github.com>
2019-09-03 17:56:57 +0200

    Update tpot_best_pipeline.py

--- personality_traits/tpot_best_pipeline.py
+++ personality_traits/tpot_best_pipeline.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from tpot.builtins import OneHotEncoder
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +12,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.7007104406450181
+# Average CV score on the training set was:0.8165207884215757
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=GaussianNB()),
-    GaussianNB()
+    OneHotEncoder(minimum_fraction=0.05, sparse=False, threshold=10),
+    StandardScaler(),
+    LogisticRegression(C=0.01, dual=True, penalty="l2")
 )
 
 exported_pipeline.fit(training_features, training_target)
