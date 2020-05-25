cbe2c13ea1ecb0a5f00e7e241cbf3e6f2e9492b7
rian-van-den-ander <52826135+rian-van-den-ander@users.noreply.github.com>
2020-05-25 10:35:07 +0200

    aita

--- aita/aita_cnn.py
+++ aita/aita_cnn.py
@@ -13,7 +13,8 @@ from sklearn.model_selection import train_test_split
 from keras.layers import Dropout
 from keras.models import Sequential
 from keras import layers
-
+from keras import optimizers
+from sklearn.metrics import classification_report, confusion_matrix
 
 def create_embedding_matrix(filepath, word_index, embedding_dim):
     vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
@@ -30,6 +31,12 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
     return embedding_matrix
 
 df = pd.read_csv('../../data/aita/aita_clean.csv')
+
+""" Delete many not_asshole items to balance dataset """
+df_asshole = df[df['is_asshole'] == 1]
+df_not_asshole =df[df['is_asshole'] == 0]
+df_not_asshole = df_not_asshole[:26511]
+df = df_not_asshole.append(df_asshole)
 
 X = df['body'].values
 X= X.astype(str)
@@ -55,8 +62,8 @@ X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
 
 input_dim = X_train.shape[1] 
 
-embedding_dim = 300
-embedding_matrix = create_embedding_matrix('../../data/embedding/glove/glove.6B.300d.txt', tokenizer.word_index, embedding_dim)
+embedding_dim = 50
+embedding_matrix = create_embedding_matrix('../../data/embedding/glove/glove.6B.50d.txt', tokenizer.word_index, embedding_dim)
 
 nonzero_elements = np.count_nonzero(np.count_nonzero(embedding_matrix, axis=1))
 embedding_accuracy = nonzero_elements / vocab_size
@@ -64,15 +71,24 @@ print('embedding accuracy: ' + str(embedding_accuracy))
 
 model = Sequential()
 model.add(layers.Embedding(vocab_size, embedding_dim, weights=[embedding_matrix], input_length=maxlen, trainable=True))
-model.add(layers.Conv1D(64, 5, activation='relu'))
-model.add(Dropout(0.7))
+model.add(layers.Conv1D(256, 5, activation='relu'))
+model.add(Dropout(0.3))
 model.add(layers.GlobalMaxPooling1D())
 model.add(layers.Dense(10, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+opt = optimizers.Adam(lr=0.0001)
+model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
 
-res = model.fit(X_train, y_train, epochs=20, verbose=True, validation_data=(X_test, y_test), batch_size=50)
+res = model.fit(X_train, y_train, epochs=50, verbose=True, validation_data=(X_test, y_test), batch_size=100)
+
+Y_pred = model.predict(X_test)
+y_pred = np.argmax(Y_pred, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(y_test, y_pred))
+print('Classification Report')
+target_names = ['not_asshole','asshole']
+print(classification_report(y_test, y_pred, target_names=target_names))
 
 loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
 print("Training Accuracy: {:.4f}".format(accuracy))
