86f21eac8a9c55d755de62388a6ffc1663fbd936
rian-van-den-ander <52826135+rian-van-den-ander@users.noreply.github.com>
2020-07-26 17:20:19 +0200

    emotions

--- emotions/emotions_cnn.py
+++ emotions/emotions_cnn.py
@@ -31,7 +31,13 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
 
     return embedding_matrix
 
-df = pd.read_csv('../../data/emotions/goemotions_1.csv')
+df1 = pd.read_csv('../../data/emotions/goemotions_1.csv')
+df2 = pd.read_csv('../../data/emotions/goemotions_2.csv')
+df3 = pd.read_csv('../../data/emotions/goemotions_3.csv')
+
+frames = [df1, df2, df3]
+
+df = pd.concat(frames)
 
 X = df['text'].values
 X= X.astype(str)
@@ -51,7 +57,7 @@ X_train = tokenizer.texts_to_sequences(X_train)
 X_test = tokenizer.texts_to_sequences(X_test)
 vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
 
-maxlen = 1000
+maxlen = 20
 X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
 X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
 
@@ -71,16 +77,16 @@ model.add(Dropout(0.2))
 model.add(layers.GlobalMaxPooling1D())
 model.add(layers.Dense(28, activation='sigmoid'))
 opt = optimizers.Adam(lr=0.0002)
-model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=opt, loss='binary_crossentropy')
 model.summary()
 
 callbacks = [EarlyStopping(monitor='val_loss', patience=2),
          ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True)]
-res = model.fit(X_train, y_train, epochs=8, verbose=True, callbacks=callbacks, validation_data=(X_test, y_test), batch_size=100)
+res = model.fit(X_train, y_train, epochs=15, verbose=True, callbacks=callbacks, validation_data=(X_test, y_test), batch_size=100)
 
 y_pred = model.predict(X_test)
 
-thresholds=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+thresholds=[0.1,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 for val in thresholds:
     pred=y_pred.copy()
   
@@ -91,6 +97,25 @@ for val in thresholds:
     recall = recall_score(y_test, pred, average='micro')
     f1 = f1_score(y_test, pred, average='micro')
    
-    print("Micro-average quality numbers")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+    print("Threshold: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(val, precision, recall, f1))
 
+column_names = list(df.columns[9:])
+f1_scores = []
+threshold = 0.25
+for i in range(0,28):
+    emotion_prediction = y_pred[:,i]
+    emotion_prediction[emotion_prediction>=threshold]=1
+    emotion_prediction[emotion_prediction<threshold]=0
+    emotion_test = y_test[:,i]
+    precision = precision_score(emotion_test, emotion_prediction)
+    recall = recall_score(emotion_test, emotion_prediction)
+    f1 = f1_score(emotion_test, emotion_prediction)
+    f1_scores.append(f1)
+    print("Emotion: {}, Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(column_names[i], precision, recall, f1))
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+plt.bar(column_names,f1_scores)
+plt.xticks(rotation=90)
+plt.show()
